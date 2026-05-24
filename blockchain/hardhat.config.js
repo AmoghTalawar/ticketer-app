2a6bf25b7cdc5e47dae1ac4ce6d61e04de3769f1
Amogh Talawar <princeamogh7@gmail.com>
2026-05-24 18:16:16 +0530

    fix: change chainId from 31337 to 1337 to avoid MetaMask Polygon Amoy conflict

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -21,11 +21,11 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      chainId: 31337,
+      chainId: 1337,
     },
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337,
+      chainId: 1337,
     },
     amoy: {
       url: process.env.POLYGON_AMOY_RPC || "https://rpc-amoy.polygon.technology",
--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -6,4 +6,4 @@ export const CONTRACT_ADDRESSES = {
   TicketMarketplace: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
 };
 
-export const CHAIN_ID = 31337;
+export const CHAIN_ID = 1337;
